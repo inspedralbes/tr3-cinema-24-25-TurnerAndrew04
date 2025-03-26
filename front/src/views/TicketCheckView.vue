13df9555381fb0cec42785c4114744a0b5d71d24
TurnerAndrew04 <a23andturmat@inspedralbes.cat>
2025-03-26 08:11:48 +0100

    arreglo

--- front/src/views/TicketCheckView.vue
+++ front/src/views/TicketCheckView.vue
@@ -53,7 +53,7 @@ const checkTickets = async () => {
       <form @submit.prevent="checkTickets" class="mb-8">
         <div class="mb-4">
           <label class="block text-gray-700 mb-2">Email</label>
-          <input v-model="email" type="email" required class="w-full px-3 py-2 border rounded">
+          <input v-model="email" type="email" required class="w-full px-3 py-2 border rounded text-black font-bold">
         </div>
         <button type="submit" class="bg-blue-600 text-white px-6 py-2 rounded hover:bg-blue-700">
           Consultar
